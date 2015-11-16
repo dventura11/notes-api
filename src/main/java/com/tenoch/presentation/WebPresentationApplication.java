368d01b1034f05d026020d1d12707406ac5e67e6
Diego Ventura <diego@dventura.net>
2015-11-16 00:59:36 -0600

    initial import

---
+++ src/main/java/com/tenoch/presentation/WebPresentationApplication.java
@@ -0,0 +1,12 @@
+package com.tenoch.presentation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebPresentationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebPresentationApplication.class, args);
+    }
+}
